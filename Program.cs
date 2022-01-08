21813bd90c96dd23105f8386e57c9196f44db701
Sukalyan <sukalyandash1920@gmail.com>
2022-01-08 18:04:36 +0530

    Line Comparision Programs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //UC1 uc = new UC1();
+            //uc.Length();
+            //UC2_LengthUsingEquals uc1 = new UC2_LengthUsingEquals();
+            //uc1.Equals();
+            UC3_LenthComparision uc2 = new UC3_LenthComparision();
+            uc2.Comparision();
+            
+        }
+    }
+}
